f26c0714f1034d0f2c1c1eb8f030c2acaad86ee7
Chandra Adhikari <chandra.adhikari1994@gmail.com>
2018-10-16 16:08:27 +0530

    changes in view page for display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Tail::Engine, at: "/tail"
+  mount Tail::Engine, at: "/log"
 	  root :to => 'logs#index'
   # Tail::Engine.routes.draw do
 	  resources :logs, only: :index
